102624f49d906235e26d28e5c2dbd145b7332d57
William <williamliususanto@gmail.com>
2025-10-17 10:00:06 +0700

    Add sorting functionality for coins by price and market cap

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,25 @@ const App = () => {
   const [error, setError] = useState<string | null>(null);
   const [pageSize, setPageSize] = useState<number>(10);
   const [search, setSearch] = useState<string>("");
+  const [orderBy, setOrderBy] = useState<string>("priceAsc");
 
   const filteredCoins =
     search === "" ? coins : coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
+
+  const orderedByCoins = (() => {
+    switch (orderBy) {
+      case "priceAsc":
+        return [...filteredCoins].sort((a, b) => a.current_price - b.current_price);
+      case "priceDesc":
+        return [...filteredCoins].sort((a, b) => b.current_price - a.current_price);
+      case "marketAsc":
+        return [...filteredCoins].sort((a, b) => a.market_cap - b.market_cap);
+      case "marketDesc":
+        return [...filteredCoins].sort((a, b) => b.market_cap - a.market_cap);
+      default:
+        return filteredCoins;
+    }
+  })();
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -57,6 +73,15 @@ const App = () => {
     <>
       <h1>React Crypto</h1>
 
+      <div className="">
+        <select value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
+          <option value="priceAsc">Price: Low to High</option>
+          <option value="priceDesc">Price: High to Low</option>
+          <option value="marketAsc">Market: Low to High</option>
+          <option value="marketDesc">Market: High to Low</option>
+        </select>
+      </div>
+
       <PageSize pageSize={pageSize} setPageSize={setPageSize}></PageSize>
 
       <SearchInput search={search} setSearch={setSearch}></SearchInput>
@@ -66,7 +91,7 @@ const App = () => {
 
       {!isLoading && !error && (
         <main className="grid">
-          {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
+          {orderedByCoins.length > 0 ? orderedByCoins.map((coin) => (
             <CoinCard key={coin.id} coin={coin}></CoinCard>
           )) : <p>No matching coins!</p>}
         </main>
